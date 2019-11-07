f113f61e6319c1d44c25616fac16980d3268f3cb
TalissonMelo <talissonmelorodrigues@gmail.com>
2019-11-07 10:46:46 -0300

    Implementação do findById

--- src/model/dao/DaoFactory.java
+++ src/model/dao/DaoFactory.java
@@ -1,11 +1,12 @@
 package model.dao;
 
+import db.DB;
 import model.dao.Impl.SellerDaoJDBC;
 
 public class DaoFactory {
 	
 	public static SellerDAO createSellerDao() {
-		return new SellerDaoJDBC();
+		return new SellerDaoJDBC(DB.getConnection());
 	}
 
 }
